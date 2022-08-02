3e7379a7424d35b2feeef09a2b1d2768ce17560b
Yakshit Jain <yakshit@criodo.com>
2022-08-02 17:57:16 +0550

    updated changes - 2022-08-02 17:37:16

--- java/src/main/java/com/crio/xcompany/company/Employee.java
+++ java/src/main/java/com/crio/xcompany/company/Employee.java
@@ -1,21 +1,12 @@
 package com.crio.xcompany.company;
 
  import java.util.Collections;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Employee {
     private String name;
     private Gender gender;
-    private Employee manager;
-    private Map<String, Employee> directReports;
 
     public Employee(String name, Gender gender) {
-        this.name = name;
-        this.gender = gender;
-        directReports = new LinkedHashMap<String, Employee>();
     }
 
     public String getName() {
@@ -24,20 +15,6 @@ public class Employee {
 
     public Gender getGender() {
         return gender;
-    }
-
-    public List<Employee> getDirectReports() {
-     return Collections.emptyList();
-    }
-
-    public List<Employee> getTeamMates() {
-     return Collections.emptyList();
-    }
-
-    public void assignManager(Employee employee){
-    }
-
-    private void addDirectReport(Employee employee){
     }
 
     @Override
--- python/src/company/Employee.py
+++ python/src/company/Employee.py
@@ -6,9 +6,6 @@ class Employee:
     def __init__(self,name: str , gender: Gender) -> None:
         self._name = name
         self._gender = gender
-        self._manager = None
-        self._direct_reports = {}
-        pass
 
     def get_name(self) -> str:
         return self._name
@@ -16,17 +13,6 @@ class Employee:
     def get_gender(self) -> str:
         return self._gender
 
-    def get_direct_reports(self) -> List:
-        pass
-
-    def get_team_mates(self) -> List:
-        pass
-
-    def assign_manager(self,employee) -> None:
-        pass
-
-    def _add_direct_report(self,employee) -> None:
-        pass
 
     def __repr__(self) -> str:
         return f'Employee [name={self._name}, gender={self._gender.value}]'
